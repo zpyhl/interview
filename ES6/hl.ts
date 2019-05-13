eb94f59f54eab84eb82242fbcbcc2e1d4a8a683c
hanlei <hl@qq.com>
2019-05-13 22:28:38 +0800

    'init'

---
+++ ES6/hl.ts
@@ -0,0 +1,16 @@
+interface Hero {
+    name: string;
+    age: number;
+    sink?: string;
+}
+
+
+
+const hero: Hero[] = [
+    {name: 'name', age: 12},
+    {name: 'name', age: 12, sink: 'yellow'},
+    {name: 'name', age: 12, sink: 'yellow'},
+]
+add(name: string = 'hl', age?:number) {
+
+}
